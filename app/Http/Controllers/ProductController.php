eaca06ae44d28e08394a74e3242e616bd6021f60
Ronny <daveavrosia@gmail.com>
2023-02-02 05:15:35 +0800

    Add product module

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'data' => Product::with([])->get(),
+            'status' => 'success',
+            'message' => 'Get product success',
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|integer'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => 'The form is not valid',
+            ]);
+        }
+
+        try {
+            DB::beginTransaction();
+
+            $data = Product::create([
+                'name' => $request->name,
+                'description' => $request->description,
+                'price' => $request->price,
+            ]);
+
+
+            DB::commit();
+        } catch (\Exception $e) {
+
+            DB::rollback();
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => 'Create product failed',
+            ]);
+        }
+
+        return response()->json([
+            'data' => [$data],
+            'status' => 'success',
+            'message' => 'Create product success',
+        ]);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|integer'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => 'The form is not valid',
+            ]);
+        }
+
+        try {
+            DB::beginTransaction();
+            $data = Product::find($id);
+
+            if ($data == null) {
+                return response()->json([
+                    'data' => [],
+                    'status' => 'failed',
+                    'message' => 'Product not found',
+                ]);
+            }
+
+            $data->name = $request->get('name');
+            $data->description = $request->get('description');
+            $data->price = $request->get('price');
+
+            $data->save();
+            DB::commit();
+        } catch (\Exception $e) {
+
+            DB::rollback();
+
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => $e,
+            ]);
+        }
+
+        return response()->json([
+            'data' => [$data],
+            'status' => 'success',
+            'message' => 'Update product success',
+        ]);
+    }
+
+    public function show($id)
+    {
+        return response()->json([
+            'data' => [Product::with([])->find($id)],
+            'status' => 'success',
+            'message' => 'Get product success',
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $data = Product::find($id);
+
+        if ($data == null) {
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => 'Product not found',
+            ]);
+        }
+        $data->delete();
+
+        return response()->json([
+            'data' => [],
+            'status' => 'success',
+            'message' => 'Delete product success',
+        ]);
+    }
+}
