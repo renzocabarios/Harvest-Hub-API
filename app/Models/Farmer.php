82af2e246b5b23f4edde72f43782b681cd74bb57
Ronny <daveavrosia@gmail.com>
2023-02-08 02:17:35 +0800

    Add farmer migration

---
+++ app/Models/Farmer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Farmer extends Model
+{
+    use HasFactory;
+
+    public $primarykey = 'id';
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+}
