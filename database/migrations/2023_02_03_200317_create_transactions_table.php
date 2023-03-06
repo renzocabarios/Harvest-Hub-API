a040a00c0b17ea24987158dfea185a0601ab50d3
renzocabarios <renzocabarios@gmail.com>
2023-03-06 16:13:25 +0800

    Update transaction migrations

--- database/migrations/2023_02_03_200317_create_transactions_table.php
+++ database/migrations/2023_02_03_200317_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,9 +14,10 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('customer');
-            $table->string('approved');
-            $table->string('confirmed');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers')->cascadeOnDelete();
+            $table->boolean('approved');
+            $table->boolean('confirmed');
             $table->timestamps();
         });
     }
