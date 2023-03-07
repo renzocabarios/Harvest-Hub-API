6a13d00e2a6121c6ba8a60e657d60782ceca9de6
renzocabarios <renzocabarios@gmail.com>
2023-03-07 19:18:18 +0800

    Update Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -19,4 +19,9 @@ class Customer extends Model
     {
         return $this->belongsTo(User::class, "user_id");
     }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class, "id");
+    }
 }
