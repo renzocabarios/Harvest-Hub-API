8f8a04685e17e12cff20e67b4bf40315a23d475e
renzocabarios <renzocabarios@gmail.com>
2023-03-11 14:55:33 +0800

    Add AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (
+            !auth()->attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ])
+        ) {
+
+            return response()->json([
+                'data' => [],
+                'status' => 'failed',
+                'message' => 'Credentials not match',
+            ]);
+        }
+
+        $token = auth()->user()->createToken('MyApp')->accessToken;
+
+        return response()->json([
+            'data' => [User::with(["farmer", "customer"])->find(auth()->user()["id"])],
+            'status' => 'success',
+            'message' => 'Authentication success',
+            'meta' => [
+                'token' => $token,
+            ]
+        ]);
+    }
+}
